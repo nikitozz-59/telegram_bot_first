79d7feb90e0f8459a85af1c0cad5e2f6077765b6
nikitozz-59 <nikitozz52@gmail.com>
2020-05-14 19:39:09 +0500

    'TeleBot.py'

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python first_bot.py
+web: python TeleBot.py
---
+++ TeleBot.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('1172370412:AAFjoR8RvgOQby1b0ZzZb9Y2smt3jZMMMKE')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+
+bot.polling(none_stop=True, interval=0)
